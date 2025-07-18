b334a8af27227969ac2b334daa29102c62dde50b
Ercode Techoglu <ercode@pm.me>
2025-07-19 00:30:35 +0300

    fix: socket settings

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,3 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+export const socket = io("http://localhost:3000", {
+  transports: ["websocket"],
+  autoConnect: false,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  timeout: 20000,
+});
