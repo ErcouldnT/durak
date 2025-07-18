371635311b5be230948be963ce03c35f64da2268
Ercode Techoglu <ercode@pm.me>
2025-07-19 00:33:36 +0300

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,9 +2,9 @@ import { io } from "socket.io-client";
 
 export const socket = io("http://localhost:3000", {
   transports: ["websocket"],
-  autoConnect: false,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 1000,
-  reconnectionDelayMax: 5000,
-  timeout: 20000,
+  // autoConnect: false,
+  // reconnectionAttempts: 5,
+  // reconnectionDelay: 1000,
+  // reconnectionDelayMax: 5000,
+  // timeout: 20000,
 });
