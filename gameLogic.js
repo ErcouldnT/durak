987ee46cbf9d9e55db0cf182f69ca0f15394c695
Ercode Techoglu <ercode@pm.me>
2025-07-24 08:49:52 +0300

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -81,7 +81,7 @@ class Durak {
     );
     const defender = this.players.get(defenderId);
     const attackCount = this.tableCards.length;
-    const maxAllowed = Math.min(6, defender.hand.length);
+    const maxAllowed = Math.min(12, defender.hand.length * 2);
 
     // Prevent too many attacks
     if (attackCount >= maxAllowed) return null;
